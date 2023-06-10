00d6c0aa491db00155d333b9ed886b1643c70eba
mrfisap <135975504+mrfisap@users.noreply.github.com>
2023-06-10 07:13:14 +0700

    Add files via upload

---
+++ hasil.php
@@ -0,0 +1,23 @@
+<?php
+$identitas=$_POST['identitas'];
+echo "Nim anda adalah <b> $identitas </b><br>";
+
+$nama=$_POST['nama'];
+echo "Nama anda adalah <b> $nama </b><br>";
+
+$teknik=$_POST['teknik'];
+echo "Jurusan anda adalah <b> $teknik </b><br>";
+
+$agama=$_POST['agama'];
+echo "Agama anda adalah <b>
+$agama </b><br>";
+
+$alamat=$_POST['alamat'];
+echo "Alamat anda adalah <b> $alamat </b><br>";
+
+$telepon=$_POST['telepon'];
+echo "Telepon anda adalah <b> $telepon </b><br>";
+
+$hobi=$_POST['hobi'];
+echo "Hobi anda adalah <b> $hobi </b><br>";
+?>
